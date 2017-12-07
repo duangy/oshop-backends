08da110e4d5b8d07cd24f00329e9983f788e3512
guangyao <478983852@qq.com>
2017-12-07 18:09:10 +0800

    add test

---
+++ oshop/products/api/test.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+#from django.core.urlresolvers import reverse
+from rest_framework.reverse import reverse
+
+
+class ProductsTest(TestCase):
+    def test_category_list(self):
+        response = self.client.get(reverse('products_api.category_list_create'))
+        self.assertEqual(response.status_code, 200)
