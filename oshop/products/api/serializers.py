8546c5c3fb514a171348292e9db8b602ef59931c
guangyao <478983852@qq.com>
2018-01-05 17:53:29 +0800

    add shopping-cart model

--- oshop/products/api/serializers.py
+++ oshop/products/api/serializers.py
@@ -14,7 +14,24 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class CartProductRelSerializer(serializers.Serializer):
+    product = serializers.StringRelatedField()
+    cart = serializers.StringRelatedField()
+    quantity = serializers.IntegerField()
+
+
 class ShoppingCartSerializer(serializers.ModelSerializer):
+    products = CartProductRelSerializer(many=True, read_only=True)
+
     class Meta:
         model = models.ShoppingCart
         fields = '__all__'
+    
+    def create(self, validated_data):
+        products_data = validated_data.pop('products') 
+        cart = models.ShoppingCart.objects.create(**validated_data)
+        all_products = []
+        for product_data in products_data:
+            all_products.append(models.CartProductRel(**product_data))
+        models.CartProductRel.objects.bulk_create(all_products)
+        return cart
--- oshop/products/models.py
+++ oshop/products/models.py
@@ -30,12 +30,25 @@ class Product(models.Model):
 
 class ShoppingCart(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    products = models.ManyToManyField(Product, through='CartProductRel', through_fields=('cart', 'product'))
     user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-        return self.id 
+        return str(self.id)
 
     class Meta:
         verbose_name = verbose_name_plural = '购物车'
+
+
+class CartProductRel(models.Model):
+    product = models.ForeignKey(Product, verbose_name='商品')
+    cart = models.ForeignKey(ShoppingCart, verbose_name='购物车')
+    quantity = models.IntegerField('数量', default=1)
+
+    def __str__(self):
+        return str(self.product)
+
+    class Meta:
+        verbose_name = verbose_name_plural = '购物车商品'
